1460993c03058636e4581489ac86eb5933d6673c
Jazz Rudolph <jazzrudolph92@outlook.com>
2023-08-01 20:40:50 -0400

    Project set up.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import ContactsForm from './components/ContactsForm/ContactsForm';
+import ContactsList from './components/ContactsList/ContactsList';
+
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  function loadInfo() {
+    fetch("https://w94wdk-8080.csb.app/api/list/")
+      .then(x => x.json())
+      .then(response => {
+        setContacts(response);
+      });
+  }
+
+  useEffect(loadInfo, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactsForm />
+      <ContactsList contacts={contacts} />
     </div>
   );
 }
---
+++ src/components/ContactsList/ContactsList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContactsListItem from './ContactsListItem';
+
+export default function ContactsList({ contacts }) {
+    let ContactsItemsJSX = contacts.map(contacts => <ContactsListItem contacts={contacts} key={contacts.id} />)
+
+    return (
+        <table>
+            <thread>
+                <tr>
+                    <th>Name</th>
+                    <th>Number</th>
+                    <th>Email</th>
+                </tr>
+            </thread>
+            <tbody>
+                {ContactsItemsJSX}
+            </tbody>
+        </table>
+    )
+}
